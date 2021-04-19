993deffd114f1ee770fe3dcebc4c7be77209e9b2
MR <masoudrajabpour@gmail.com>
2021-04-19 16:57:37 +0430

    Init

---
+++ MR.TVOSToast/MR_TVOSToast.h
@@ -0,0 +1,18 @@
+//
+//  MR_TVOSToast.h
+//  MR.TVOSToast
+//
+//  Created by MR on 4/19/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MR_TVOSToast.
+FOUNDATION_EXPORT double MR_TVOSToastVersionNumber;
+
+//! Project version string for MR_TVOSToast.
+FOUNDATION_EXPORT const unsigned char MR_TVOSToastVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MR_TVOSToast/PublicHeader.h>
+
+
